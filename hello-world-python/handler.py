193682c28a82b2988cec94a5b85d4163fb4776d2
Cameron Beeler <cameron.beeler@gmail.com>
2020-06-30 01:04:28 -0700

    Added another function

--- hello-world-python/handler.py
+++ hello-world-python/handler.py
@@ -4,3 +4,7 @@ def hello(event, context):
     print("Second Update!")
     print("Third Update!")
     return "This is the return statement from the hello method"
+
+def greetings(event, context):
+    print("Greetings, Cameron!")
+    return "This is a test of a multiple function lambda"
